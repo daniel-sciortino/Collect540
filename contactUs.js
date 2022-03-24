c1ab881b190e7a7b260adfc8651050c5299fe527
daniel-sciortino <77982896+daniel-sciortino@users.noreply.github.com>
2022-03-23 20:17:11 -0400

    Add files via upload

---
+++ contactUs.js
@@ -0,0 +1,16 @@
+function someFunction() {
+    document.getElementById("home-container").style.visibility = "hidden";
+    document.getElementById("send-email-container").style.visibility = "visible";
+
+    console.log("working");
+}
+
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    var userName= e.target.elements.name.value;
+    var userEmail= e.target.elements.email.value;
+    var userMessage= e.target.elements.message.value;
+console.log(userMessage)
+  
+  });
