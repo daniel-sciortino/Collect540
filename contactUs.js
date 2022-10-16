623f2e219fe77a01b5c9c7d88e43f41a127bd7b2
“Daniel <daniel.sciortino@gmail.com>
2022-10-16 11:54:41 -0400

    fixed links + contact page

--- contactUs.js
+++ contactUs.js
@@ -1,16 +1,14 @@
 function popUpForm() {
-    document.getElementById("home-container").style.visibility = "hidden";
-    document.getElementById("send-email-container").style.visibility = "visible";
+  document.getElementById("home-container").style.visibility = "hidden";
+  document.getElementById("send-email-container").style.visibility = "visible";
 
-    console.log("working");
+  console.log("working");
 }
 
-
-document.querySelector('#contact-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-    var userName= e.target.elements.name.value;
-    var userEmail= e.target.elements.email.value;
-    var userMessage= e.target.elements.message.value;
-console.log(userMessage)
-  
-  });
+document.querySelector("#contact-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  var userName = e.target.elements.name.value;
+  var userEmail = e.target.elements.email.value;
+  var userMessage = e.target.elements.message.value;
+  console.log(userMessage);
+});
